00847d6ca510cee77c0fe1bbdc94f8bb12fd2861
Dmitry Rybalchenko <rybalchenko.it@gmail.com>
2022-08-18 20:28:54 +0300

    Added render and reset metods, meals, check-function for touch walls and the body, now the game works. Also added score and name for players

--- script.js
+++ script.js
@@ -17,40 +17,117 @@ document.addEventListener('keydown', (event) => {
 	if (event.code === 'ArrowLeft') snake.turnLeft();
 })
 
+const maxCoord = 500 - 10;
+let speed = 100;
+let turnOnce = 1;
+let scoreCounter = 0;
+let userName;
+let userScore;
+
 const snake = {
-	body: [
-		{ coordX: 130, coordY: 100 },
-		{ coordX: 120, coordY: 100 },
-		{ coordX: 110, coordY: 100 },
-		{ coordX: 100, coordY: 100 },
-	],
+	body: [],
 	direction: 'right',
-	meal: {},
-	turnUp() { this.direction = 'up' },
-	turnRight() { this.direction = 'right' },
-	turnDown() { this.direction = 'down' },
-	turnLeft() { this.direction = 'left' },
+	meal: { coordX: 0, coordY: 0 },
+
+	turnUp() { if (this.direction !== 'down' && turnOnce) { this.direction = 'up'; turnOnce = 0; } },
+	turnRight() { if (this.direction !== 'left' && turnOnce) { this.direction = 'right'; turnOnce = 0; } },
+	turnDown() { if (this.direction !== 'up' && turnOnce) { this.direction = 'down'; turnOnce = 0; } },
+	turnLeft() { if (this.direction !== 'right' && turnOnce) { this.direction = 'left'; turnOnce = 0; } },
+
+	NewBlock(x, y) { return { coordX: x, coordY: y } },
+
+	checkBody(x, y) {
+		for (let block of this.body) {
+			if (block.coordX === x && block.coordY === y) return true;
+		}
+	},
+
+	go() { this.stopId = setInterval(() => { this.render(); }, speed); },
+	stopId: null,
+	stop() { clearInterval(this.stopId) },
+
+	addMeal() {
+		this.meal.coordX = Math.round(Math.floor(Math.random() * (maxCoord + 1)) / 10) * 10;
+		this.meal.coordY = Math.round(Math.floor(Math.random() * (maxCoord + 1)) / 10) * 10;
+		if (this.checkBody(this.meal.coordX, this.meal.coordY)) this.addMeal;
+	},
 };
 
 snake.render = function () {
+	turnOnce = 1;
 	let direction = this.direction;
-	let coordXHead = this.body[0].coordX;
-	let coordYHead = this.body[0].coordY;
-	// checkSmash
+	let coordXHead = this.body.at(-1).coordX;
+	let coordYHead = this.body.at(-1).coordY;
+
+	//Coords new block
 	if (direction === 'up') coordYHead += 10;
 	if (direction === 'right') coordXHead += 10;
 	if (direction === 'down') coordYHead -= 10;
 	if (direction === 'left') coordXHead -= 10;
-	snake.body.unshift({ coordX: coordXHead, coordY: coordYHead });
 
-	// checkMeal
-	snake.body.pop()
+	//checking walls
+	if (coordXHead < 0) coordXHead = maxCoord;
+	if (coordXHead > maxCoord) coordXHead = 0;
+	if (coordYHead < 0) coordYHead = maxCoord;
+	if (coordYHead > maxCoord) coordYHead = 0;
 
-	//checkWall
+	// checking smash
+	if (this.checkBody(coordXHead, coordYHead)) {
+		this.stop();
+		field.classList.add('snake-game__body_fail');
+		setTimeout(() => { this.reset(); }, 3000)
+
+	}
+
+	snake.body.push(this.NewBlock(coordXHead, coordYHead));
+
+	// checking meal
+	if (this.meal.coordX === coordXHead && this.meal.coordY === coordYHead) {
+		this.addMeal();
+		scoreCounter += 10;
+	} else {
+		snake.body.shift();
+	}
+
 	field.innerHTML = '';
-	snake.body.forEach(item => {
-		field.insertAdjacentHTML('beforeend', `<div class="snake-game__item" style="left: ${item.coordX}px; bottom: ${item.coordY}px;"></div>`)
-	});
+	this.body.forEach(item => displayBlock(item));
+	displayBlock(this.meal);
 
+	function displayBlock(block) {
+		field.insertAdjacentHTML('beforeend', `<div class="snake-game__item" style="left: ${block.coordX}px; bottom: ${block.coordY}px;"></div>`)
+	}
 }
 
+snake.reset = function () {
+	field.classList.remove('snake-game__body_fail');
+
+	if (localStorage.getItem('userName') === null) {
+		userName = prompt('Введите ваше имя игрока');
+		if (+userName === 0) userName = `user${Math.round(Math.random() * 1000)}`;
+		localStorage.setItem('userName', userName);
+	}
+	if (localStorage.getItem('userName') === null) {
+		localStorage.setItem('userScore', scoreCounter);
+	} else {
+		userScore = localStorage.getItem('userScore');
+		if (scoreCounter > userScore) localStorage.setItem('userScore', scoreCounter);
+	}
+
+	// adding body snake
+	this.body.length = 0;
+	let center = (maxCoord + 10) / 2;
+	for (let i = 0; i <= 40; i += 10) {
+		this.body.push(this.NewBlock(center + i, center));
+	}
+
+	this.direction = 'right';
+	this.addMeal();
+	this.go();
+
+	console.log(localStorage.getItem('userName'));
+	console.log(localStorage.getItem('userScore'));
+}
+
+window.onload = () => {
+	snake.reset();
+}
