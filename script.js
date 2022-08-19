0d52b543816ad7292151dcdde9b96a93ee0102f9
Dmitry Rybalchenko <rybalchenko.it@gmail.com>
2022-08-19 11:23:50 +0300

    Refactoring, adding comments

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const field = document.querySelector('.snake-game__body');
 
+// Processing control commands
 document.querySelector('.snake-game').addEventListener('mousedown', (event) => {
 	let targetClass = event.target.classList;
 	if (targetClass.contains('snake-game__btn--up')) snake.turnUp();
@@ -19,20 +20,27 @@ document.addEventListener('keydown', (event) => {
 
 const maxCoord = 500 - 10;
 let speed = 100;
-let turnOnce = 1;
-let scoreCounter = 0;
+let turnOnce = true;
+let userScore = 0;
 let userName;
-let userScore;
+let userMaxScore;
 
+window.onload = () => { snake.reset(); }
+
+// Main snake object
 const snake = {
 	body: [],
 	direction: 'right',
-	meal: { coordX: 0, coordY: 0 },
+	meal: {},
+	stopId: null,
 
-	turnUp() { if (this.direction !== 'down' && turnOnce) { this.direction = 'up'; turnOnce = 0; } },
-	turnRight() { if (this.direction !== 'left' && turnOnce) { this.direction = 'right'; turnOnce = 0; } },
-	turnDown() { if (this.direction !== 'up' && turnOnce) { this.direction = 'down'; turnOnce = 0; } },
-	turnLeft() { if (this.direction !== 'right' && turnOnce) { this.direction = 'left'; turnOnce = 0; } },
+	turnUp() { if (this.direction !== 'down' && turnOnce) { this.direction = 'up'; turnOnce = false; } },
+	turnRight() { if (this.direction !== 'left' && turnOnce) { this.direction = 'right'; turnOnce = false; } },
+	turnDown() { if (this.direction !== 'up' && turnOnce) { this.direction = 'down'; turnOnce = false; } },
+	turnLeft() { if (this.direction !== 'right' && turnOnce) { this.direction = 'left'; turnOnce = false; } },
+
+	go() { this.stopId = setInterval(() => { this.render(); }, speed); },
+	stop() { clearInterval(this.stopId) },
 
 	NewBlock(x, y) { return { coordX: x, coordY: y } },
 
@@ -42,10 +50,6 @@ const snake = {
 		}
 	},
 
-	go() { this.stopId = setInterval(() => { this.render(); }, speed); },
-	stopId: null,
-	stop() { clearInterval(this.stopId) },
-
 	addMeal() {
 		this.meal.coordX = Math.round(Math.floor(Math.random() * (maxCoord + 1)) / 10) * 10;
 		this.meal.coordY = Math.round(Math.floor(Math.random() * (maxCoord + 1)) / 10) * 10;
@@ -54,41 +58,42 @@ const snake = {
 };
 
 snake.render = function () {
-	turnOnce = 1;
+	turnOnce = true;
 	let direction = this.direction;
 	let coordXHead = this.body.at(-1).coordX;
 	let coordYHead = this.body.at(-1).coordY;
 
-	//Coords new block
+	// Coords new head block
 	if (direction === 'up') coordYHead += 10;
 	if (direction === 'right') coordXHead += 10;
 	if (direction === 'down') coordYHead -= 10;
 	if (direction === 'left') coordXHead -= 10;
 
-	//checking walls
+	// Checking walls
 	if (coordXHead < 0) coordXHead = maxCoord;
 	if (coordXHead > maxCoord) coordXHead = 0;
 	if (coordYHead < 0) coordYHead = maxCoord;
 	if (coordYHead > maxCoord) coordYHead = 0;
 
-	// checking smash
+	// Checking smash
 	if (this.checkBody(coordXHead, coordYHead)) {
 		this.stop();
 		field.classList.add('snake-game__body_fail');
 		setTimeout(() => { this.reset(); }, 3000)
-
 	}
 
+	// Adding new head
 	snake.body.push(this.NewBlock(coordXHead, coordYHead));
 
-	// checking meal
+	// Checking meal and deleting old tail
 	if (this.meal.coordX === coordXHead && this.meal.coordY === coordYHead) {
 		this.addMeal();
-		scoreCounter += 10;
+		userScore += 10;
 	} else {
 		snake.body.shift();
 	}
 
+	// Rendering html
 	field.innerHTML = '';
 	this.body.forEach(item => displayBlock(item));
 	displayBlock(this.meal);
@@ -101,33 +106,30 @@ snake.render = function () {
 snake.reset = function () {
 	field.classList.remove('snake-game__body_fail');
 
+	// Adding user name and max user score in local storage
 	if (localStorage.getItem('userName') === null) {
 		userName = prompt('Введите ваше имя игрока');
 		if (+userName === 0) userName = `user${Math.round(Math.random() * 1000)}`;
 		localStorage.setItem('userName', userName);
 	}
 	if (localStorage.getItem('userName') === null) {
-		localStorage.setItem('userScore', scoreCounter);
+		localStorage.setItem('userMaxScore', userScore);
 	} else {
-		userScore = localStorage.getItem('userScore');
-		if (scoreCounter > userScore) localStorage.setItem('userScore', scoreCounter);
+		userMaxScore = localStorage.getItem('userMaxScore');
+		if (userScore > userMaxScore) localStorage.setItem('userMaxScore', userScore);
+		userScore = 0;
 	}
 
-	// adding body snake
+	// Reset: adding new body snake, direction, meal and startinh new game
 	this.body.length = 0;
 	let center = (maxCoord + 10) / 2;
 	for (let i = 0; i <= 40; i += 10) {
 		this.body.push(this.NewBlock(center + i, center));
 	}
-
 	this.direction = 'right';
 	this.addMeal();
 	this.go();
 
 	console.log(localStorage.getItem('userName'));
-	console.log(localStorage.getItem('userScore'));
-}
-
-window.onload = () => {
-	snake.reset();
+	console.log(localStorage.getItem('userMaxScore'));
 }
